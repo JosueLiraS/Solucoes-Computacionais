5d95a3a8c7fe535f27ae7085a60c3d768bcbb404
JosueLiraS <161076393+JosueLiraS@users.noreply.github.com>
2024-06-10 15:33:27 -0300

    Update and rename Exercício 7 to Moto.java

---
+++ Terceira Lista/Moto.java
@@ -0,0 +1,12 @@
+
+package com.projetos.principal;
+
+public class Moto extends Veiculo {
+
+    @Override
+    public void mover() {
+        System.out.println("A moto esta se movendo");
+
+    }
+    
+}
