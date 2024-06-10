cce34b573cd69945bde7c474ba0d87f79c4729d5
JosueLiraS <161076393+JosueLiraS@users.noreply.github.com>
2024-06-10 15:31:44 -0300

    Update and rename Exercício 4 to Carro.java

---
+++ Terceira Lista/Carro.java
@@ -0,0 +1,12 @@
+
+package com.projetos.principal;
+
+
+public class Carro extends Veiculo {
+
+    @Override
+    public void mover() {
+        System.out.println("O carro esta se movendo");
+    }
+    
+}
